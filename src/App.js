0f75d1adf787f1d12c9e5f377eaa7d82a77686ad
Rishi Purwar <57035408+rishipurwar1@users.noreply.github.com>
2022-03-14 11:29:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import "./App.css";
+
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+          <span role="img" aria-label="waving hand">
+            👋
+          </span>{" "}
+          Hey there!
+        </div>
+
+        <div className="bio">
+          I am Rishi and I am building a Web3 App with Solidity + Ethereum Smart
+          Contracts so that's pretty cool right? Connect your Ethereum wallet
+          and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
